c84da12a3d72b405f1834968e1f14897511f9e57
Shashank Muthuraj <shashankm2325@gmail.com>
2024-07-31 20:45:39 -0500

    minor change

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyClQmKCrkfOhFoFkWilR5JzTzCCjm70ias",
+    authDomain: "portfolio-cfe2f.firebaseapp.com",
+    projectId: "portfolio-cfe2f",
+    storageBucket: "portfolio-cfe2f.appspot.com",
+    messagingSenderId: "854976857635",
+    appId: "1:854976857635:web:2440e092313729bc335396",
+    measurementId: "G-TYJW0BB9ZV"
+};
+
+export default firebaseConfig;
